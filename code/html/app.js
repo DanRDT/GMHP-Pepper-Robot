2e929fa4178d52d323022196d580158267352911
unknown <pulberdan@gmail.com>
2023-06-10 15:37:53 -0400

    jquery components for food and category cards

--- code/html/app.js
+++ code/html/app.js
@@ -1,23 +1,33 @@
-import { menuItems } from './javascript/data.js'
+import { menuItems, dailySpecials, foodCategories } from './javascript/data.js'
+import { itemCard, categoryCard } from './javascript/jquery-components/main-menu-page.js'
 // import { QiSessionConnection } from './javascript/qiClass.js'
 import { QiSessionConnection } from './javascript/qiClassTesting.js'
 
 // modify the DOM (Website)
-$(document).ready(function () {
+// Shorthand for $(document).ready()
+$(function () {
   // connect to robot
   const session = new QiSessionConnection()
 
   // alert(JSON.stringify(menuItems))
 
+  // pages setup
+  dailySpecials.map(item => {
+    $('#food-items-container').append(itemCard(item.name, item.price, item.calories, item.image))
+  })
+  foodCategories.map(category => {
+    $('#food-categories-container').append(categoryCard(category.name, category.image))
+  })
+
   // open main menu page
-  $('#start-order-btn').click(function () {
+  $('#start-order-btn').on('click', function () {
     // alert('starting order')
     $('#start-page').hide()
     $('#main-menu-page').css('display', 'flex')
   })
 
   // return to start page
-  $('#main-menu-page .cancel-btn').click(function () {
+  $('#main-menu-page .cancel-btn').on('click', function () {
     $('#start-page').css('display', 'flex')
     $('#main-menu-page').hide()
   })
---
+++ code/html/javascript/jquery-components/main-menu-page.js
@@ -0,0 +1,23 @@
+export function itemCard(itemName, price, calories, image) {
+  return `
+  <div class="food-card">
+    <img class="image" src="./resources/images/${image}" alt="" />
+    <div class="item-details">
+      <p>${price}</p>
+      <p class="text-nowrap">${calories} cal</p>
+    </div>
+    <div class="flex-grow"></div>
+    <p class="text-center font-semibold">${itemName}s</p>
+  </div> 
+  `
+}
+export function categoryCard(categoryName, image) {
+  return `
+  <div class="food-categories-card">
+    <img class="image" src="./resources/images/${image}" alt="" />
+    <p class="font-semibold text-center">${categoryName}</p>
+    <div class="flex-grow"></div>
+    <img class="arrow-in-circle" src="./resources/images/other/arrow-in-circle.svg" alt=">" />
+  </div>
+  `
+}
