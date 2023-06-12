ee6f4276a8858afed12756b4deb480caad4956dc
unknown <pulberdan@gmail.com>
2023-06-11 23:18:30 -0400

    test code for speech and face regognition

--- code/html/javascript/qiClass.js
+++ code/html/javascript/qiClass.js
@@ -1,3 +1,4 @@
+import { secs } from './utils/global.js'
 import { getRandomPepperDialog } from './utils/pepper.js'
 
 export class QiSessionConnection {
@@ -52,10 +53,47 @@ export class QiSessionConnection {
    * @param {boolean} wordSpotting
    */
   speechRecognition(phrases, wordSpotting) {
+    // connect to ALMemory
+    this.#session.service('ALMemory').then(function (ALMemory) {
+      // connect to event listener
+      ALMemory.subscriber('WordRecognized').then(function (subscriber) {
+        // subscribe to event
+        // `.signal` required?
+        subscriber.signal.connect(function (value, subscriberIdentifier) {})
+      })
+    })
+
+    this.#session.service('ALMemory').subscriber('FaceDetected').signal.connect(callbackFunc)
     this.#session.service('ALSpeechRecognition').then(function (asr) {
       this.#speechListener = asr
       this.#speechListener.setVocabulary(phrases, wordSpotting)
+      callback(eventName, value, 'User')
     })
+  }
+  #startListening() {
+    this.#speechListener.subscribe('User')
+  }
+  stopListening() {
+    this.#speechListener.unsubscribe('User')
+  }
+  restartListener() {
+    this.#speechListener.pause(false)
+  }
+
+  faceDetection(phrases, wordSpotting) {
+    this.ALMemory = this.#session.service('ALMemory')
+    this.subscriber = this.ALMemory.subscriber('FaceDetected')
+    this.subscriber.signal.connect(callbackFunc)
+
+    this.#session.service('ALFaceDetection').subscribe('HumanGreeter')
+    this.got_face = false
+
+    setInterval(() => {
+      // only detect a face every n seconds
+      this.got_face = false
+    }, secs(5))
+
+    this.face_detection.unsubscribe('HumanGreeter')
   }
 
   /**
@@ -74,3 +112,42 @@ export class QiSessionConnection {
     this.performSpeech(dialog)
   }
 }
+
+// function onServiceAdded(serviceId, serviceName) {
+//   console.log('New service', serviceId, serviceName)
+//   ALM.serviceAdded.disconnect(signalLink)
+// }
+
+// session.service('ServiceDirectory').then(function (ALM) {
+//   ALM = sd
+//   ALM.serviceAdded.connect(onServiceAdded).then(
+//     function (link) {
+//       signalLink = link
+//     },
+//     function (error) {
+//       console.log('An error occurred: ' + error)
+//     }
+//   )
+// })
+
+// session.service('ALMemory').then(function (ALMemory) {
+//   // connect to event listener
+//   ALMemory.subscriber('WordRecognized').then(function (subscriber) {
+//     // subscribe to event
+//     // `.signal` required?
+//     subscriber.signal
+//       .connect(function (value, subscriberIdentifier) {
+//         setTimeout(() => {
+//           subscriber.signal.disconnect(signalLink)
+//         }, secs(15))
+//       })
+//       .then(
+//         function (link) {
+//           signalLink = link
+//         },
+//         function (error) {
+//           alert('An error occurred: ' + error)
+//         }
+//       )
+//   })
+// })
