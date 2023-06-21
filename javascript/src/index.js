71798f760ad116c39f7cda4514bb6e870d8c3b55
kagreen93 <kanng1993@gmail.com>
2023-06-21 17:43:02 -0400

    updated to show category items

--- javascript/src/index.js
+++ javascript/src/index.js
@@ -1,7 +1,7 @@
 // @ts-check
-import { menuItems, dailySpecials, foodCategories } from './data/menu.js'
+import { menuItems, dailySpecials, foodCategories, sides, desserts, beverages, coffeeItems, healthyOptions } from './data/menu.js'
 import { itemCard, categoryCard } from './jquery-components/main-menu-page.js'
-// import { QiSessionConnection } from './qiClass.js'
+//import { QiSessionConnection } from './qiClass.js'
 import { secs } from './utils/global.js'
 import { QiSessionConnection } from './qiClassTesting.js'
 import { navigateToPage } from './utils/pages.js'
@@ -24,6 +24,34 @@ $(function () {
   foodCategories.map(category => {
     $('#food-categories-container').append(categoryCard(category.name, category.image))
   })
+
+  // Define object to story category information
+  var categoryInfo = {
+    'Daily Specials': dailySpecials,
+    'Sides': sides,
+    'Desserts': desserts,
+    'Beverages': beverages,
+    'Coffee': coffeeItems,
+    'Healthy Options': healthyOptions
+  }
+  
+  // Show food items when category is clicked
+  $('#food-categories-container').on('click', '.food-categories-card',function() {
+    // Access the clicked image using $(this)
+    var clickedImage = $(this)
+    var divId = clickedImage.attr('id')
+    
+    // Clear the #food-items-container before appending item cards from new category
+    $('#food-items-container').empty()
+    
+    // Append item cards for the specified category
+    categoryInfo[divId].forEach(item => {
+      $('#food-items-container').append(itemCard(item.name, item.price, item.calories, item.image))
+    })
+  })
+
+
+
 
   // class A {
   //   string = 'hey'
--- javascript/src/jquery-components/main-menu-page.js
+++ javascript/src/jquery-components/main-menu-page.js
@@ -2,20 +2,20 @@
 
 export function itemCard(itemName, price, calories, image) {
   return `
-  <div class="food-card">
+  <div id="${itemName}" class="food-card">
     <img class="image" src="./resources/images/${image}" alt="" />
     <div class="item-details">
       <p>${price}</p>
       <p class="text-nowrap">${calories} cal</p>
     </div>
     <div class="flex-grow"></div>
-    <p class="text-center font-semibold">${itemName}s</p>
+    <p class="text-center font-semibold">${itemName}</p>
   </div> 
   `
 }
 export function categoryCard(categoryName, image) {
   return `
-  <div class="food-categories-card">
+  <div id="${categoryName}" class="food-categories-card">
     <img class="image" src="./resources/images/${image}" alt="" />
     <p class="font-semibold text-center">${categoryName}</p>
     <div class="flex-grow"></div>
