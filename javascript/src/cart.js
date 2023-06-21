f391d4fe90de51a187cbf6d5000bcf0f5c6c03ec
Alvaro Huerta <alvarohuerta2430@gmail.com>
2023-06-21 13:21:50 -0400

    Cart class

--- javascript/src/cart.js
+++ javascript/src/cart.js
@@ -1,9 +1,31 @@
-class Cart{
-    cart = []
-    constructor (){}
-    getCart(){}
-    addToCart(){}
-    removeFromCart(){}
-    clearCart(){}
-
+class Cart {
+    constructor() {
+          this.cart = [];
+    }
+      
+    getCart() {
+          return this.cart;
+    }
+      
+    addToCart(item) {
+          this.cart.push(item);
+          console.log(`Added ${item} to the cart.`);
+    }
+      
+    removeFromCart(item) {
+          const index = this.cart.indexOf(item);
+          if (index !== -1) {
+            this.cart.splice(index, 1);
+            console.log(`Removed ${item} from the cart.`);
+          } else {
+            console.log(`${item} is not in the cart.`);
+          }
+    }
+      
+      clearCart() {
+          this.cart = [];
+          console.log('Cart cleared.');
+    }
 }
+      
+
