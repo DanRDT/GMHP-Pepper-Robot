68fc1be7ad929ace1489c199dd63db73b4ba4c2e
kagreen93 <kanng1993@gmail.com>
2023-07-17 14:30:33 -0400

    updated removeFromCart.js

--- javascript/src/voiceAssistant/removeFromCart.js
+++ javascript/src/voiceAssistant/removeFromCart.js
@@ -24,37 +24,60 @@ export function removeItemFromCart(cart, session) {
       newUserChat(capitalize(response))
 
       const itemToRemove = response.toLowerCase()
-      // Compare the response to menu item names
-      const menuItem = menuItems.find(item => item.name.toLowerCase() === itemToRemove)
-      // Item found in the menu items array
+      // Pull current cart
+      const myCart = cart.getCart()
+        
+      // Filter cart items that match the specified name
+      const matchingItems = myCart.filter(item => item.name.toLowerCase() === itemToRemove)
 
-      if (menuItem) {
-        // Check if the item exists in the cart
-        const cartItems = cart.getCart()
-        const itemIndex = cartItems.findIndex(item => item.name.toLowerCase() === itemToRemove)
-
-        if (itemIndex !== -1) {
-          // Item found in the cart, remove it
-          const [removedItem] = cartItems.splice(itemIndex, 1)
-          session.performSpeech(`The ${removedItem.name} has been removed from your cart.`)
-          newRobotChat(`The ${removedItem.name} has been removed from your cart.`)
-        } else {
-          // Item not found in the cart
-          session.performSpeech(`Sorry, ${itemToRemove} not found in your cart.`)
-          newRobotChat(`Sorry, ${itemToRemove} not found in your cart.`)
-        }
-      } else {
-        // No match found in the menu items array
-        session.performSpeech(`Sorry, I don't recognize that item.`)
-        newRobotChat(`Sorry, I don't recognize that item.`)
+      if (matchingItems.length === 0) {
+        // Item not found in the cart
+        session.performSpeech(`Sorry, ${itemToRemove} not found in your cart.`)
+        newRobotChat(`Sorry, ${itemToRemove} not found in your cart.`)
         cancelVoiceAssistant()
+        return
       }
-    }
-  )
+
+      if (matchingItems.length === 1) {
+        // Single variant item found in the cart, remove it
+        const [removedItem] = myCart.splice(myCart.indexOf(matchingItems[0]), 1)
+        session.performSpeech(`The ${removedItem.name} has been removed from your cart.`)
+        newRobotChat(`The ${removedItem.name} has been removed from your cart.`)
+        return
+      }
+
+      // Multiple variant items found in the cart, prompt for the variant
+      const variants = matchingItems.map(item => item.variant.toLowerCase())
+      session.performSpeech(`Which option of ${itemToRemove} would you like to remove?`)
+      clearUserOptions()
+      newRobotChat(`Which variant of ${itemToRemove} would you like to remove?`)
+        session.setSpeechRecognitionFunc(
+          /** @param {[string, number]} data */
+          ([variantResponse, variantConfidence]) => {
+            if (variantConfidence < 0.45) return
+
+          const variantToRemove = variantResponse.toLowerCase()
+          const itemIndex = myCart.findIndex(item => item.name.toLowerCase() === itemToRemove && item.variant.toLowerCase() === variantToRemove)
+
+          if (itemIndex !== -1) {
+            // Item found in the cart, remove it
+            const [removedItem] = myCart.splice(itemIndex, 1);
+            session.performSpeech(`The ${removedItem.name} (${removedItem.variant}) has been removed from your cart.`)
+            newRobotChat(`The ${removedItem.name} (${removedItem.variant}) has been removed from your cart.`)
+          } else {
+            // Item not found in the cart
+            session.performSpeech(`Sorry, ${itemToRemove} (${variantToRemove}) not found in your cart.`)
+            newRobotChat(`Sorry, ${itemToRemove} (${variantToRemove}) not found in your cart.`)
+          }})
+        })
 
   // Phrases to listen for
   const menuItemsArray = menuItems.map(item => item.name.toLowerCase())
-  const phrases = ['nevermind', 'cancel', ...menuItemsArray]
+  const variantArray = menuItems.map(variants => variants.name.toLowerCase())
+  const phrases = ['nevermind', 'cancel', ...menuItemsArray, ...variantArray]
   session.listenForPhrases(phrases, false, 25)
   newUserOptions(phrases)
 }
+
+
+
